7289755900e916fe2c18fb1a936a576426918b7f
Ayama11 <ayamalla882@gmail.com>
2023-12-10 14:55:55 +0300

    add circular chip for home view

--- lib/views/drugs_view.dart
+++ lib/views/drugs_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:repopharma_app/help/const.dart';
+import 'package:repopharma_app/widgets/circular_container_shep.dart';
+import 'package:repopharma_app/widgets/custom_clip.dart';
 
 class Drugs extends StatelessWidget {
   const Drugs({super.key});
@@ -6,16 +9,39 @@ class Drugs extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: const Color.fromARGB(255, 149, 203, 247),
-        height: 300,
-        width: 300,
-        child: const Center(
-            child: Text(
-          'durg',
-          style: TextStyle(fontSize: 20, color: Colors.red),
-        )),
+        body: SingleChildScrollView(
+      child: Column(
+        children: [
+          ClipPath(
+            clipper: CustomClipP(),
+            child: Container(
+              color: kBaseColor,
+              padding: const EdgeInsets.all(0),
+              child: SizedBox(
+                height: 350,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: -100,
+                      right: -250,
+                      child: CircularContainer(
+                        backgroundcolor: Colors.white.withOpacity(0.1),
+                      ),
+                    ),
+                    Positioned(
+                      top: 100,
+                      right: -300,
+                      child: CircularContainer(
+                        backgroundcolor: Colors.white.withOpacity(0.1),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-    );
+    ));
   }
 }
